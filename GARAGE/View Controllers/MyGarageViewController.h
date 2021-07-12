bfd0e58d5d6ba231e1ceaa367952151e1abd64b0
Sanjana Meduri <sanjana.meduri@gmail.com>
2021-07-12 13:10:09 -0400

    added logout feature

---
+++ GARAGE/View Controllers/MyGarageViewController.h
@@ -0,0 +1,16 @@
+//
+//  MyGarageViewController.h
+//  GARAGE
+//
+//  Created by Sanjana Meduri on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyGarageViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GARAGE/View Controllers/MyGarageViewController.m
@@ -0,0 +1,44 @@
+//
+//  MyGarageViewController.m
+//  GARAGE
+//
+//  Created by Sanjana Meduri on 7/12/21.
+//
+
+#import "MyGarageViewController.h"
+#import "Parse/Parse.h"
+#import "LoginViewController.h"
+#import "SceneDelegate.h"
+
+
+@interface MyGarageViewController ()
+
+@end
+
+@implementation MyGarageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)onLogout:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {}];
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    SceneDelegate *myDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    myDelegate.window.rootViewController = loginViewController;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
