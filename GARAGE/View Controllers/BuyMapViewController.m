af52282f87895154599cc4038a82a790264ae540
Sanjana Meduri <sanjana.meduri@gmail.com>
2021-07-14 14:27:41 -0400

    added marker on item address

--- GARAGE/View Controllers/BuyMapViewController.m
+++ GARAGE/View Controllers/BuyMapViewController.m
@@ -10,6 +10,9 @@
 
 @interface BuyMapViewController ()
 
+@property (strong, nonatomic) GMSMapView *mapView;
+@property (strong, nonatomic) NSDictionary *destinationCoordinates;
+
 @end
 
 @implementation BuyMapViewController
@@ -20,24 +23,71 @@
     [self setEdgesForExtendedLayout:UIRectEdgeNone];
     [self setAutomaticallyAdjustsScrollViewInsets:NO];
     
-    //following snippet from Google Maps guide
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86 longitude:151.20 zoom:6];
-    GMSMapView *mapView = [GMSMapView mapWithFrame:self.view.frame camera:camera];
-    mapView.myLocationEnabled = YES;
+    self.mapView.settings.myLocationButton = YES;
+    self.mapView.myLocationEnabled = YES;
     
-    //following snipped from stack overflow
+    [self geocodeRequest];
+}
+
+- (void) setupMap{
+    double latitude = [self.destinationCoordinates[@"lat"] doubleValue];
+    double longitude = [self.destinationCoordinates[@"lng"] doubleValue];
+    
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:latitude longitude:longitude zoom:6];
+    self.mapView = [GMSMapView mapWithFrame:self.view.frame camera:camera];
+    self.mapView.myLocationEnabled = YES;
+
+    
     CGRect f = self.view.frame;
     CGRect mapFrame = CGRectMake(f.origin.x, 44, f.size.width, f.size.height);
-    mapView = [GMSMapView mapWithFrame:mapFrame camera:camera];
-    [self.view addSubview:mapView];
+    self.mapView = [GMSMapView mapWithFrame:mapFrame camera:camera];
+    [self.view addSubview:self.mapView];
     
-    // Creates a marker in the center of the map.
-    GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
-    marker.title = @"Sydney";
-    marker.snippet = @"Australia";
-    marker.map = mapView;
+    GMSMarker *marker = [GMSMarker markerWithPosition:CLLocationCoordinate2DMake(latitude, longitude)];
+    marker.title = @"Item Address";
+    marker.map = self.mapView;
+}
+
+- (void) geocodeRequest{
+    NSString *urlString = [[@"https://maps.googleapis.com/maps/api/geocode/json?address=" stringByAppendingString:self.listing.address] stringByAppendingString:@"&key=AIzaSyB3uTAyKC64dPGL_nnZpz0KPcm0PpFXyNc"];
     
+    urlString = [urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    
+    NSURL *requestUrl = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:requestUrl cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+               if (error != nil) {
+                   NSLog(@"%@", [error localizedDescription]);
+
+                   UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Network Connection Failed" message:@"It looks like you are not connected to the Internet! Please check your connection and try again." preferredStyle:UIAlertControllerStyleAlert];
+
+                   UIAlertAction *retryAction = [UIAlertAction actionWithTitle:@"Retry" style:UIAlertActionStyleDefault handler:^(UIAlertAction *_Nonnull action){
+                       [self geocodeRequest];
+                   }];
+
+                   [alert addAction:retryAction];
+
+                   UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *_Nonnull action){}];
+
+                   [alert addAction:cancelAction];
+
+                   [self presentViewController:alert animated:YES completion:^{}];
+               }
+               else {
+                   //Get the array of movies
+                   NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+                
+                   self.destinationCoordinates = dataDictionary[@"results"][0][@"geometry"][@"location"];
+                   
+                   [self setupMap];
+                
+                   NSLog(@"%@", dataDictionary[@"results"][0][@"geometry"][@"location"]);
+               }
+           }];
+        [task resume];
+
 }
 
 /*
