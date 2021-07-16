a29ffc979d79dade672b274ae5773cc94923c3b2
Sanjana Meduri <sanjana.meduri@gmail.com>
2021-07-16 12:15:52 -0400

    added utils class

--- GARAGE/Utils/utils.h
+++ GARAGE/Utils/utils.h
@@ -6,10 +6,16 @@
 //
 
 #import <Foundation/Foundation.h>
+@import Parse;
+#import "Parse/Parse.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface utils : NSObject
+
+@property (assign, nonatomic) int numListings;
+
++ (PFQuery*) setUpQuery;
 
 @end
 
--- GARAGE/Utils/utils.m
+++ GARAGE/Utils/utils.m
@@ -9,4 +9,27 @@
 
 @implementation utils
 
+int numListings = 20;
+
++ (PFQuery*) setUpQuery{
+    PFQuery *query = [PFQuery queryWithClassName:@"Listing"];
+    [query orderByDescending:@"createdAt"];
+    [query includeKey:@"seller"];
+    [query includeKey:@"description"];
+    [query includeKey:@"alreadySold"];
+    [query includeKey:@"inInventory"];
+    [query includeKey:@"createdAt"];
+    [query includeKey:@"tag"];
+    [query includeKey:@"name"];
+    [query includeKey:@"condition"];
+    [query includeKey:@"image"];
+    [query includeKey:@"address"];
+    [query includeKey:@"price"];
+    [query includeKey:@"itemEmail"];
+    
+    query.limit = numListings;
+    
+    return query;
+}
+
 @end
