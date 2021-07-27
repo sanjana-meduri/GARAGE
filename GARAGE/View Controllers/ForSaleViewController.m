ec3c8c1c189b894c0caf2f90d187a6ed58aea0f1
Sanjana Meduri <sanjana.meduri@gmail.com>
2021-07-27 12:24:27 -0400

    styled for sale page

--- GARAGE/View Controllers/ForSaleViewController.m
+++ GARAGE/View Controllers/ForSaleViewController.m
@@ -25,10 +25,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+
+    gradient.frame = self.view.bounds;
+    UIColor *lightColor = [UIColor colorWithRed: 0.75 green: 0.91 blue: 0.91 alpha: 1.00];
+    UIColor *darkColor = [UIColor colorWithRed: 0.38 green: 0.71 blue: 0.80 alpha: 1.00];
+    gradient.colors = @[(id)lightColor.CGColor, (id)darkColor.CGColor];
+
+    [self.view.layer insertSublayer:gradient atIndex:0];
+    
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     
-    self.tableView.backgroundColor = [UIColor systemBlueColor];
+    self.tableView.backgroundColor = [UIColor clearColor];
     [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     
     self.user = PFUser.currentUser;
